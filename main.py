c115b686f993d8a8302e49f736b91988708b836a
Hillstar <61278508+Hillstar@users.noreply.github.com>
2022-10-24 16:53:51 +0700

    Minor refactoring and improvements

--- main.py
+++ main.py
@@ -20,12 +20,16 @@ updater = Updater(token,
 
 j = updater.job_queue
 
-register_cmd = "Зарегистрироваться"
-unregister_cmd = "Отменить регистрацию"
+register_cmd = "Зарегистрироваться \U0001F385"
+unregister_cmd = "Отменить регистрацию \U0001F44E"
 show_participators_cmd = "Показать участников"
+help_cmd = "Помощь \U00002753"
 
 my_tz = timezone('Etc/GMT-7')
-send_time = dt.datetime(2022, 10, 22, 1, 13, 0, 000000, tzinfo=my_tz)
+send_time = dt.datetime(2022, 10, 24, 16, 52, 0, 000000, tzinfo=my_tz)
+
+buttons = [[KeyboardButton(register_cmd), KeyboardButton(unregister_cmd)], 
+    [KeyboardButton(help_cmd), KeyboardButton(show_participators_cmd)]]
 
 #users = [(1, 'ALEX', 'ALEX_NICK22222212'), (2, 'TOM', 'TOM_NICK22222212'), (3, 'JIM', 'JIM_NICK22222212'), (4, 'SARA', 'SARA_NICK22222212')]
 users = []
@@ -34,16 +38,9 @@ def id_list_debug():
     print(f"current id list:\n {users}" )
 
 def start(update: Update, context: CallbackContext):
-    buttons = [[KeyboardButton(register_cmd), KeyboardButton(unregister_cmd)], [KeyboardButton(show_participators_cmd)]]
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to my bot!", reply_markup=ReplyKeyboardMarkup(buttons))
     update.message.reply_text(
-        "Привет! Для участия в Тайном Санте от тебя требуется лишь нажать кнопку 'Зарегистрироваться'. В свое время тебе придет имя человека, который будет ждать от тебя подарок")
-
-def help(update: Update, context: CallbackContext):
-    buttons = [[KeyboardButton(register_cmd), KeyboardButton(unregister_cmd)], [KeyboardButton(show_participators_cmd)]]
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to my bot!", reply_markup=ReplyKeyboardMarkup(buttons))
-    update.message.reply_text(
-        "Чтобы принять участие в Тайном Санте, тебе нужно нажать кнопку 'Зарегистрироваться'. В любой момент ты можешь отменить свою регистрацию кнопкой 'Отменить регистрацию'")
+        "Привет! Для участия в Тайном Санте от тебя требуется лишь нажать кнопку 'Зарегистрироваться'. В свое время тебе придет имя человека, который будет ждать от тебя подарок",
+        reply_markup=ReplyKeyboardMarkup(buttons, resize_keyboard=True))
 
 def generate(context: CallbackContext):
     #print("generation begins")
@@ -73,6 +70,7 @@ def message_handler(update: Update, context: CallbackContext):
         else:
             update.message.reply_text("Ты уже зарегистрирован")
         #id_list_debug()
+
     elif update.message.text == unregister_cmd:
         if user_info in users:
             users.remove((update.message.chat_id, str(update.message.from_user.first_name), str(update.message.from_user.username)))
@@ -80,19 +78,26 @@ def message_handler(update: Update, context: CallbackContext):
         else:
             update.message.reply_text("Ты не зарегистрирован")
         #id_list_debug()
+
     elif update.message.text == show_participators_cmd:
         for user in users:
             update.message.reply_text(f"@{user[2]}")
+
+    elif update.message.text == help_cmd:
+        update.message.reply_text(
+            "Чтобы принять участие в Тайном Санте, тебе нужно нажать кнопку 'Зарегистрироваться'. В любой момент ты можешь выйти из игры кнопкой 'Отменить регистрацию'",
+            reply_markup=ReplyKeyboardMarkup(buttons, resize_keyboard=True))
+
     else:
         update.message.reply_text(
-            "Sorry I can't recognize you, you said '%s'" % update.message.text)
+            "Прости, я не понимаю текст. Пользуйся кнопками, плз", 
+            reply_markup=ReplyKeyboardMarkup(buttons, resize_keyboard=True))
   
 def unknown(update: Update, context: CallbackContext):
     update.message.reply_text(
         "Sorry '%s' is not a valid command" % update.message.text)
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('help', help))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
 
 # Filters out unknown commands
